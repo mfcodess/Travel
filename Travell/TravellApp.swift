6775b04d0e9244349e4cf8432b9083368dc75ccf
Maxim Fedoseenko <158294939+mfcodess@users.noreply.github.com>
2026-09-11 20:34:14 +0300

    Initial Commit

---
+++ Travell/TravellApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellApp.swift
+//  Travell
+//
+//  Created by Максим  on 11.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct TravellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
